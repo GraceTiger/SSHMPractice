5ae4542b6d1baea0da7cef8fb3b9d767595a0d02
xiuhu <1043152224@qq.com>
2020-01-17 19:04:40 +0800

    添加Spring

---
+++ Spring01/src/main/java/com/zxc/service/impl/AccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zxc.service.impl;
+
+import com.zxc.dao.IAccountDao;
+import com.zxc.service.IAccountService;
+
+/**
+ * 账户的业务层实现类
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao ;
+
+    public AccountServiceImpl(){
+        System.out.println("对象创建了");
+    }
+
+    public void  saveAccount(){
+        accountDao.saveAccount();
+    }
+}
